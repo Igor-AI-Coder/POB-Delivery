b98e9f89e658fc1fec08d4f6da3b7c22130ac886
emanoel <eoemanoel@gmail.com>
2026-03-29 13:56:05 -0300

    feat: criar modelos de Produto, Cliente e Pedido

--- Sistema de Delivery/src/modelo/Cliente.java
+++ Sistema de Delivery/src/modelo/Cliente.java
@@ -1,5 +1,52 @@
 package modelo;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Cliente {
+    private String nome;
+    private String endereco;
+    private List<Pedido> pedidos;
 
+    public Cliente(String nome, String endereco) {
+        this.nome = nome;
+        this.endereco = endereco;
+        this.pedidos = new ArrayList<>();
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public List<Pedido> getPedidos() {
+        return pedidos;
+    }
+
+    public void adicionarPedido(Pedido pedido) {
+        this.pedidos.add(pedido);
+    }
+
+    public void removerPedido(Pedido pedido) {
+        this.pedidos.remove(pedido);
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" +
+                "nome='" + nome + '\'' +
+                ", endereco='" + endereco + '\'' +
+                ", pedidos=" + pedidos +
+                '}';
+    }
 }
--- Sistema de Delivery/src/modelo/Pedido.java
+++ Sistema de Delivery/src/modelo/Pedido.java
@@ -1,5 +1,65 @@
 package modelo;
 
-public class Pedido {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Pedido {
+    private int id;
+    private String data;
+    private Cliente cliente;
+    private List<Produto> produtos;
+
+    public Pedido(int id, String data, Cliente cliente) {
+        this.id = id;
+        this.data = data;
+        this.cliente = cliente;
+        this.produtos = new ArrayList<>();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public List<Produto> getProdutos() {
+        return produtos;
+    }
+
+    public void adicionarProduto(Produto produto) {
+        this.produtos.add(produto);
+    }
+
+    public void removerProduto(Produto produto) {
+        this.produtos.remove(produto);
+    }
+
+    
+    @Override
+    public String toString() {
+        return "Pedido{" +
+                "id=" + id +
+                ", data=" + data +
+                ", cliente=" + cliente.getNome() +
+                ", produtos=" + produtos +
+                '}';
+    }
 }
